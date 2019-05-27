539bf38da2d0e49fdc1a7b56291fec656a7bbf3f
david <davidvoler@gmail.com>
2019-05-27 15:39:54 +0300

    adding some explnation to the readme

--- src/ptv_web_app/core.cljs
+++ src/ptv_web_app/core.cljs
@@ -1,6 +1,6 @@
 (ns ptv-web-app.core
-    (:require [om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]))
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
@@ -8,21 +8,20 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
-
+(defonce app-state (atom {:text "polyglotv.org!"}))
 
 (om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/div nil
-                 (dom/h1 nil (:text data))
-                 (dom/h3 nil "Edit this and watch it change!")))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+ (fn [data owner]
+   (reify om/IRender
+     (render [_]
+       (dom/div nil
+                (dom/h1 nil (:text data))
+                (dom/h3 nil "Initial polyglotv.org aplication")))))
+ app-state
+ {:target (. js/document (getElementById "app"))})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
